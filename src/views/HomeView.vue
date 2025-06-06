c43223e8b48f9f6ae83d2cece4bdb2bb3bae988d
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-06-06 15:43:36 +0200

    feat: add basic input for url

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,11 @@ import TheWelcome from '../components/TheWelcome.vue'
 
 <template>
   <main>
-    <TheWelcome />
+    <!-- Error -->
+     <p v-if="errorMessage">{{ errorMessage }}</p>
+    <div >
+      <input v-model="url" type="url" name="url" placeholder="Coller l'URL de votre vidéo Youtube">
+      <button type="submit" @click="submitDownloader">Valider</button>
+    </div>
   </main>
 </template>
