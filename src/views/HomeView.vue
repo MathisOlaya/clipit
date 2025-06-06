5b708484351508c515e9289de1b557d372a8dafe
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-06-06 15:43:53 +0200

    feat: create websocket connection on url submited

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,24 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+// Vue
+import { ref } from 'vue'
+
+// REFs
+const url = ref('')
+const errorMessage = ref('')
+
+function submitDownloader(){
+  // Set ERROR Message if URL is null
+  if(!url.value){
+    return errorMessage.value = "L'URL ne peut pas être vide"
+  }
+
+  // Else, unset error message
+  errorMessage.value = null;
+
+  // Create WebSocket Connexion
+  const ws = new WebSocket(import.meta.env.VITE_BACKEND_WS || 'ws://localhost:3000');
+  
+}
 </script>
 
 <template>
