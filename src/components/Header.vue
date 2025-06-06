608f912ddaa7f35c79a70713fc72c4035dc359c0
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-06-07 01:43:43 +0200

    feat: create header component

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<script>
+import { RouterLink } from 'vue-router'
+</script>
+<template>
+  <div class="main">
+    <RouterLink :to="{ name: 'home' }">
+      <h1>Clip It</h1>
+    </RouterLink>
+    <div>
+      <RouterLink to="pricing"> Tarifs </RouterLink>
+      <RouterLink to="login">Se connecter</RouterLink>
+    </div>
+  </div>
+</template>
+<style scoped>
+.main {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 16px;
+}
+
+.main div {
+  display: flex;
+  gap: 12px;
+}
+</style>
