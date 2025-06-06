376872dde7623f2a4cc28cf94635c89a601222f3
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-06-07 01:47:04 +0200

    feat: create component for <hr/>

---
+++ src/components/Hr.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  size: {
+    type: String,
+    default: '70%',
+  },
+})
+</script>
+<template>
+  <hr :width="size" />
+</template>
+<style>
+hr {
+  margin: 32px 0;
+  background-color: #1d2e61;
+  height: 2px;
+  border: none;
+}
+</style>
