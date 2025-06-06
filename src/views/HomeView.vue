71a6992a9c55f7cbf0e17482a36424a2502369b9
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-06-06 16:32:20 +0200

    feat: adding websocket connexion

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,6 +5,7 @@ import { ref } from 'vue'
 // REFs
 const url = ref('')
 const errorMessage = ref('')
+const statusMessage = ref('')
 
 function submitDownloader(){
   // Set ERROR Message if URL is null
@@ -13,11 +14,32 @@ function submitDownloader(){
   }
 
   // Else, unset error message
-  errorMessage.value = null;
+  errorMessage.value = '';
 
   // Create WebSocket Connexion
+  statusMessage.value = 'Connexion en cours au serveur'
   const ws = new WebSocket(import.meta.env.VITE_BACKEND_WS || 'ws://localhost:3000');
   
+  ws.onopen = () => {
+    statusMessage.value = 'Connexion au serveur établie'
+
+    // Start VIDEO Downloading
+    ws.send(JSON.stringify({ url: url.value }));
+  }
+
+  ws.onmessage = (event) => {
+    const data = JSON.parse(event.data);
+
+    // Set status message
+    statusMessage.value = data.message
+
+    if(data.type === 'done'){
+      // Save UUID 
+      localStorage.setItem('uuid-vid', data.id)
+
+      // Get COVER
+    }
+  }
 }
 </script>
 
@@ -25,9 +47,14 @@ function submitDownloader(){
   <main>
     <!-- Error -->
      <p v-if="errorMessage">{{ errorMessage }}</p>
+     <!-- Status Message -->
+      <p v-if="statusMessage">{{ statusMessage }}</p>
     <div >
       <input v-model="url" type="url" name="url" placeholder="Coller l'URL de votre vidéo Youtube">
       <button type="submit" @click="submitDownloader">Valider</button>
     </div>
+    <video controls>
+      <source src="route-vers-la-video" type="video/mp4" />
+    </video>
   </main>
 </template>
