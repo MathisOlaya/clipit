0eea0dc967e711a6e6fec30ecb4a7ee95b05ef8f
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-06-07 01:44:47 +0200

    feat(frontend): add api service for backend call

---
+++ src/services/ApiService.ts
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+const apiClient = axios.create({
+  baseURL: import.meta.env.VITE_BACKEND_HOST,
+})
+
+export default {
+  getSecondaryVideos() {
+    return apiClient.get('video/secondary')
+  },
+}
