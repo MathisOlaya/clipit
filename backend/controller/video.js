268932680860c8afddcc74f466f34ced388f60ee
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-06-06 15:19:37 +0200

    feat: adding status type on download

--- backend/controller/video.js
+++ backend/controller/video.js
@@ -1,6 +1,6 @@
 import ytdl from '@distube/ytdl-core'
 import fs from 'fs'
-import { json } from 'stream/consumers'
+import { v4 as uuidv4 } from 'uuid';
 
 class VideoController {
     constructor(){}
@@ -13,18 +13,19 @@ class VideoController {
         try{
             // Validate VIDEO's URL
             if(!ytdl.validateURL(url)){
-                return res.status(400).json({message: "L'URL fournie n'est pas valide"})
+                return res.status(400).json({message: "L'URL fournie n'est pas valide", type: 'error'})
             }
 
             // Get Youtube Video Title
-            ws.send(JSON.stringify({message: "Obtention des données de la vidéo..."}))
-            const info = await ytdl.getInfo(url)
+            ws.send(JSON.stringify({message: "Création d'un identifiant unique...", type: 'pending'}))
+            const uniqueID = uuidv4();
             
+            // Download
+            ws.send(JSON.stringify({message: "Téléchargement de la vidéo...", type: 'pending'}))
+            ytdl(url).pipe(fs.createWriteStream(`uploads/${uniqueID}.mp4`))
             
-            ws.send(JSON.stringify({message: "Téléchargement de la vidéo..."}))
-            ytdl(url).pipe(fs.createWriteStream('uploads/'.concat(info.videoDetails.title.concat('.mp4'))))
-            
-            
+            // On Download DONE
+            ws.send(JSON.stringify({message: "✅ Le téléchargement est terminé.", type: 'done', id: uniqueID }))
         } catch(err) {
             console.log(err)
             return res.status(500).json({message: "Merci de réessayer ultérieurement"})
