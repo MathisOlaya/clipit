8b783475c687fbdbad5cf447b91431fe5ff4f02e
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-06-06 17:59:59 +0200

    feat: show 10 first second of the vid in frontend

--- backend/controller/video.js
+++ backend/controller/video.js
@@ -1,101 +1,114 @@
 import ytdl from '@distube/ytdl-core'
 import fs from 'fs'
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid'
 
 // FFMPEG
 import ffmpeg from 'fluent-ffmpeg'
 import staticFfmpeg from 'ffmpeg-static'
 
 // FFMPEG Config
-ffmpeg.setFfmpegPath(staticFfmpeg);
+ffmpeg.setFfmpegPath(staticFfmpeg)
 
 class VideoController {
-    constructor(){}
-    async create(url, ws){
-        // Youtube VIDEO URL
-        if(!url){
-            return ws.send(JSON.stringify({message: "Aucune URL n'a été fournie"}))
-        }
-
-        try{
-            // Validate VIDEO's URL
-            if(!ytdl.validateURL(url)){
-                return res.status(400).json({message: "L'URL fournie n'est pas valide", type: 'error'})
-            }
-
-            // Get Youtube Video Title
-            ws.send(JSON.stringify({message: "Création d'un identifiant unique...", type: 'pending'}))
-            const uniqueID = uuidv4();
-            
-            // Download
-            ws.send(JSON.stringify({message: "Téléchargement de la vidéo...", type: 'pending'}))
-            ytdl(url).pipe(fs.createWriteStream(`uploads/${uniqueID}.mp4`))
-            
-            // On Download DONE
-            ws.send(JSON.stringify({message: "✅ Le téléchargement est terminé.", type: 'done', id: uniqueID }))
-        } catch(err) {
-            console.log(err)
-            return res.status(500).json({message: "Merci de réessayer ultérieurement"})
-        }
-        
+  constructor() {}
+  async create(url, ws) {
+    // Youtube VIDEO URL
+    if (!url) {
+      return ws.send(JSON.stringify({ message: "Aucune URL n'a été fournie" }))
     }
-    async getPreview(req, res) {
-        // Get UUID
-        const { uuid } = req.params
 
-        if(!uuid){
-            return res.status(404).json({message: "L'identifiant de la vidéo n'a pas été trouvé"})
-        }
+    try {
+      // Validate VIDEO's URL
+      if (!ytdl.validateURL(url)) {
+        return res.status(400).json({ message: "L'URL fournie n'est pas valide", type: 'error' })
+      }
 
-        // Video Path 
-        const path = `uploads/${uuid}.mp4`
-        if(!fs.existsSync(path)){
-            return res.status(404).json({message: "Aucune vidéo n'a été trouvée avec cet identifiant"})
-        }
+      // Get Youtube Video Title
+      ws.send(JSON.stringify({ message: "Création d'un identifiant unique...", type: 'pending' }))
+      const uniqueID = uuidv4()
 
-        // Get FIRST 10 seconds of the video
-        const outputPath = `uploads/${uuid}-preview.mp4`
-        ffmpeg(path).setStartTime(0).setDuration(10).output(outputPath).on('end', () => {
-            // Is video existing
-            if (!fs.existsSync(outputPath)) {
-                return res.status(404).send('Vidéo non trouvée');
-            }
+      // Download
+      const fileStream = fs.createWriteStream(`uploads/${uniqueID}.mp4`)
+      ws.send(JSON.stringify({ message: 'Téléchargement de la vidéo...', type: 'pending' }))
+      ytdl(url).pipe(fileStream)
 
-            const stat = fs.statSync(outputPath);
-            const fileSize = stat.size;
-            const range = req.headers.range;
-
-            if (range) {
-                const parts = range.replace(/bytes=/, "").split("-");
-                const start = parseInt(parts[0], 10);
-                const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
-
-                if (start >= fileSize) {
-                res.status(416).send('Requested range not satisfiable\n' + start + ' >= ' + fileSize);
-                return;
-                }
-
-                const chunksize = (end - start) + 1;
-                const file = fs.createReadStream(outputPath, { start, end });
-
-                res.writeHead(206, {
-                'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-                'Accept-Ranges': 'bytes',
-                'Content-Length': chunksize,
-                'Content-Type': 'video/mp4', // adapte selon ton format
-                });
-
-                file.pipe(res);
-            } else {
-                // Pas de range header, envoie la vidéo entière
-                res.writeHead(200, {
-                'Content-Length': fileSize,
-                'Content-Type': 'video/mp4',
-                });
-                fs.createReadStream(outputPath).pipe(res);
-            }
-        }).run()
+      // On Download DONE
+      fileStream.on('finish', () => {
+        ws.send(
+          JSON.stringify({
+            message: '✅ Le téléchargement est terminé.',
+            type: 'done',
+            id: uniqueID,
+          }),
+        )
+      })
+    } catch (err) {
+      console.log(err)
+      return res.status(500).json({ message: 'Merci de réessayer ultérieurement' })
     }
+  }
+  async getPreview(req, res) {
+    // Get UUID
+    const { uuid } = req.params
+
+    if (!uuid) {
+      return res.status(404).json({ message: "L'identifiant de la vidéo n'a pas été trouvé" })
+    }
+
+    // Video Path
+    const path = `uploads/${uuid}.mp4`
+    if (!fs.existsSync(path)) {
+      return res.status(404).json({ message: "Aucune vidéo n'a été trouvée avec cet identifiant" })
+    }
+
+    // Get FIRST 10 seconds of the video
+    const outputPath = `uploads/${uuid}-preview.mp4`
+    ffmpeg(path)
+      .setStartTime(0)
+      .setDuration(10)
+      .output(outputPath)
+      .on('end', () => {
+        // Is video existing
+        if (!fs.existsSync(outputPath)) {
+          return res.status(404).send('Vidéo non trouvée')
+        }
+
+        const stat = fs.statSync(outputPath)
+        const fileSize = stat.size
+        const range = req.headers.range
+
+        if (range) {
+          const parts = range.replace(/bytes=/, '').split('-')
+          const start = parseInt(parts[0], 10)
+          const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1
+
+          if (start >= fileSize) {
+            res.status(416).send('Requested range not satisfiable\n' + start + ' >= ' + fileSize)
+            return
+          }
+
+          const chunksize = end - start + 1
+          const file = fs.createReadStream(outputPath, { start, end })
+
+          res.writeHead(206, {
+            'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': chunksize,
+            'Content-Type': 'video/mp4', // adapte selon ton format
+          })
+
+          file.pipe(res)
+        } else {
+          // Pas de range header, envoie la vidéo entière
+          res.writeHead(200, {
+            'Content-Length': fileSize,
+            'Content-Type': 'video/mp4',
+          })
+          fs.createReadStream(outputPath).pipe(res)
+        }
+      })
+      .run()
+  }
 }
 
-export default new VideoController();
+export default new VideoController()
