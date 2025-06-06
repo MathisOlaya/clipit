4e8cf25868bdc03b5f74745dee9f9a28eced32ca
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-06-06 17:59:45 +0200

    feat: sending done message when download is finish only

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,40 +4,42 @@ import { ref } from 'vue'
 
 // REFs
 const url = ref('')
+const videoPreview = ref('')
 const errorMessage = ref('')
 const statusMessage = ref('')
 
-function submitDownloader(){
+function submitDownloader() {
   // Set ERROR Message if URL is null
-  if(!url.value){
-    return errorMessage.value = "L'URL ne peut pas être vide"
+  if (!url.value) {
+    return (errorMessage.value = "L'URL ne peut pas être vide")
   }
 
   // Else, unset error message
-  errorMessage.value = '';
+  errorMessage.value = ''
 
   // Create WebSocket Connexion
   statusMessage.value = 'Connexion en cours au serveur'
-  const ws = new WebSocket(import.meta.env.VITE_BACKEND_WS || 'ws://localhost:3000');
-  
+  const ws = new WebSocket(import.meta.env.VITE_BACKEND_WS || 'ws://localhost:3000')
+
   ws.onopen = () => {
     statusMessage.value = 'Connexion au serveur établie'
 
     // Start VIDEO Downloading
-    ws.send(JSON.stringify({ url: url.value }));
+    ws.send(JSON.stringify({ url: url.value }))
   }
 
   ws.onmessage = (event) => {
-    const data = JSON.parse(event.data);
+    const data = JSON.parse(event.data)
 
     // Set status message
     statusMessage.value = data.message
 
-    if(data.type === 'done'){
-      // Save UUID 
+    if (data.type === 'done') {
+      // Save UUID
       localStorage.setItem('uuid-vid', data.id)
 
-      // Get COVER
+      // Set Preview
+      videoPreview.value = `http://localhost:3000/video/${data.id}/preview`
     }
   }
 }
@@ -46,15 +48,20 @@ function submitDownloader(){
 <template>
   <main>
     <!-- Error -->
-     <p v-if="errorMessage">{{ errorMessage }}</p>
-     <!-- Status Message -->
-      <p v-if="statusMessage">{{ statusMessage }}</p>
-    <div >
-      <input v-model="url" type="url" name="url" placeholder="Coller l'URL de votre vidéo Youtube">
+    <p v-if="errorMessage">{{ errorMessage }}</p>
+    <!-- Status Message -->
+    <p v-if="statusMessage">{{ statusMessage }}</p>
+    <div>
+      <input
+        v-model="url"
+        type="url"
+        name="url"
+        placeholder="Coller l'URL de votre vidéo Youtube"
+      />
       <button type="submit" @click="submitDownloader">Valider</button>
     </div>
-    <video controls>
-      <source src="route-vers-la-video" type="video/mp4" />
+    <video controls v-if="videoPreview">
+      <source :src="videoPreview" type="video/mp4" />
     </video>
   </main>
 </template>
