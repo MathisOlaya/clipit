8f513a8a7c533802b6d2984c4a1aada3601ca380
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-06-21 10:58:15 +0200

    feat: add login authentication (backend)

--- backend/controller/auth.js
+++ backend/controller/auth.js
@@ -58,8 +58,51 @@ class AuthController {
     }
   }
 
+  async login(req, res) {
+    if (!req.body) {
+      return res.status(404).json({ message: 'Merci de rentrer tous les champs' })
+    }
+
+    // Creds
+    const { mail, password } = req.body
+
+    try {
+      const user = await prisma.user.findFirst({
+        where: {
+          mail,
+        },
+      })
+
+      if (!user) {
+        return res.status(404).json({ message: "L'email ou le mot de passe est incorrect" })
+      }
+
+      const result = await bcrypt.compare(password, user.password)
+
+      if (!result) {
+        return res.status(404).json({ message: "L'email ou le mot de passe est incorrect" })
+      }
+
+      // Generate JWT
+      const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET_KEY, { expiresIn: '1d' })
+
+      // Save it to cookies
+      res.cookie('token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'strict',
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+      })
+
+      return res.status(200).json({ message: 'Succès' })
+    } catch {
+      return res.status(500).json({ message: 'Veuillez réessayer utlérieurement' })
+    }
+  }
+
   async me(req, res) {
     const token = req.cookies.token
+
     if (!token) return res.status(401).json({ error: 'Non authentifié' })
 
     try {
