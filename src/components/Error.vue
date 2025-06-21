ea9b663d6c721304c04a71bfebc1936c90fb4c26
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-06-21 10:59:49 +0200

    feat: create component for displaying message error

---
+++ src/components/Error.vue
@@ -0,0 +1,31 @@
+<script setup>
+defineProps({
+  text: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+<template>
+  <div class="error">
+    <p>{{ text }}</p>
+  </div>
+</template>
+<style scoped>
+div {
+  width: 100%;
+  height: 32px;
+  border-radius: 6px;
+  background-color: #ef233c;
+  border: 1px solid #a4161a;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  opacity: 0.8;
+}
+
+p {
+  background-color: #ef233c;
+  font-size: 13px;
+}
+</style>
