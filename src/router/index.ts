c92923d6364da8ee132575117fc289435b1dd5d8
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2025-06-25 23:18:31 +0200

    fea(frontend router): redirect user to pricing component

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import DownloadView from '@/views/DownloadView.vue'
 import LoginView from '@/views/Authentication/LoginView.vue'
+import PricingView from '@/views/PricingView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: LoginView,
+    },
+    {
+      path: '/pricing',
+      name: 'pricing',
+      component: PricingView,
     },
   ],
 })
